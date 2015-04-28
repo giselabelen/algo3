710469b749a1b8bec32d1aa791fa21c28b1573d6
giselabelen <gise_5291@yahoo.com.ar>
2015-04-28 19:53:18 -0300

    bash para test de la catedra

---
+++ TP2/src/tests_catedra.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+DIR=../tests_catedra
+DIR1=$DIR/tests.tp2.ej1.v1.1
+#DIR2=$DIR/	FALTAN LOS TESTS PARA EL EJ2
+DIR3=$DIR/tests.tp2.ej3
+
+RES=../Resultados_tests_catedra
+RES1=$RES/Ej1
+RES2=$RES/Ej2
+RES3=$RES/Ej3
+
+echo "Borrando carpeta "$RES
+
+rm -r $RES
+mkdir $RES
+mkdir $RES1
+mkdir $RES2
+mkdir $RES3
+
+echo "Compilando"
+
+./compilar.sh
+
+echo "Corriendo Tests Dakkar"
+
+for file in $DIR1/*.in
+do
+	base=$(basename "$file" .in)
+	./zombieland < $file > $RES1/$base.out
+done
+
+#~ echo "Corriendo Tests Zombieland II"
+
+#~ for file in $DIR2/*.in
+#~ do
+	#~ base=$(basename "$file" .in)
+	#~ ./altafrecuencia < $file > $RES2/$base.out
+#~ done
+
+#~ echo "Corriendo Tests Petróleo"	FALTA IMPLEMENTAR
+#~ 
+#~ for file in $DIR3/*.in
+#~ do
+	#~ base=$(basename "$file" .in)
+	#~ ./caballos < $file > $RES3/$base.out
+#~ done
