0573f74174ec3c11ec27e606a4cf901f60c5336e
giselabelen <gise_5291@yahoo.com.ar>
2015-04-28 21:44:14 -0300

    parece que dakkar ya esta

--- TP2/src/dakkar.h
+++ TP2/src/dakkar.h
@@ -88,7 +88,7 @@ Carrera dakkar(int n, int km, int kb, int* bici, int* moto, int* buggy)
 	// demás posiciones
 	aux = eleccion(bici[0],moto[0],buggy[0]);
 	for(i = 1; i <= kb; i++){
-		for(j = 1; j < km; j++){
+		for(j = 1; j <= km; j++){
 			etapas[0][i][j] = aux;
 		}
 	}
@@ -108,7 +108,7 @@ Carrera dakkar(int n, int km, int kb, int* bici, int* moto, int* buggy)
 		// posiciones (1,0) a (kb,0)
 		for(i = 1; i <= kb; i++){
 			aux = eleccion(etapas[h-1][i][0].first + bici[h],-1,etapas[h-1][i-1][0].first + buggy[h]);
-			etapas[h][i][0];
+			etapas[h][i][0] = aux;
 		}
 		
 		// demás posiciones
@@ -116,9 +116,7 @@ Carrera dakkar(int n, int km, int kb, int* bici, int* moto, int* buggy)
 		{
 			for(j = 1; j <= km; j++)
 			{
-				aux = eleccion(etapas[h-1][i][j].first + bici[h],
-								etapas[h-1][i][j-1].first + moto[h],
-								etapas[h-1][i-1][j].first + buggy[h]);
+				aux = eleccion(etapas[h-1][i][j].first + bici[h],etapas[h-1][i][j-1].first + moto[h],etapas[h-1][i-1][j].first + buggy[h]);
 				etapas[h][i][j] = aux;
 			}
 		}
