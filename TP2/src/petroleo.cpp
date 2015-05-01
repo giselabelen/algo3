3266ae7308ea866513ec88c05b65d2b6060be920
giselabelen <gise_5291@yahoo.com.ar>
2015-05-01 18:48:59 -0300

    petroleo bello

--- TP2/src/petroleo.cpp
+++ TP2/src/petroleo.cpp
@@ -12,8 +12,6 @@ int main()
 	int costo_total;
 	int cant_ref = 0;
 	int cant_tub;
-	int aux1;
-	int aux2;
 	list<int> refinerias;
 	list<Arista> tuberias;
 	Arista edge;
@@ -24,68 +22,38 @@ int main()
 	scanf("%i",&m);	// cantidad de posibles tuberías
 	scanf("%i",&costo_ref);	// costo por refinería
 	
-	//~ Grafo g(n, Vec(n,-1));	// grafo - en ppio lleno de (-1) - voy a ir agregando las conexiones
+	Incidencia inc;	// lista que contendrá las posibles tuberías con sus costos
 	
-	//~ Adyacencia ady(n);	// arreglo de n elementos que contendrá los vecinos de c/u
-	Incidencia inc;	// lista que contendrá las posibles tuberías
-	
-	// lleno las estructuras
+	// lleno la lista de incidencia
 	for(int i = 0; i < m; i++)
 	{
 		scanf("%i",&t1);
 		scanf("%i",&t2);
 		scanf("%i",&costo_tub);
 		
-		if(costo_tub <= costo_ref)
-		{
-			//~ g[t1-1][t2-1] = costo_tub;
-			//~ g[t2-1][t1-1] = costo_tub;
-			
-			aux1 = min(t1,t2) - 1;
-			aux2 = max(t1,t2) - 1;
-			
-			//~ ady[aux1].push_back(aux2);
-			edge = make_pair(aux1,aux2);
-			con_peso = make_pair(edge,costo_tub);
-			inc.push_back(con_peso);
+		if(costo_tub <= costo_ref)	// si conviene usar esa tubería
+		{			
+			edge = make_pair(t1-1,t2-1);	// tubería
+			con_peso = make_pair(edge,costo_tub);	// (tubería,peso)
+			inc.push_back(con_peso);	// agrego a la lista
 		}
 	}
-	
-	// MAGIA
-	//~ cant_ref = comp_conexas(g,n,refinerias);	
-	//~ cant_tub = petroleo(g,n,tuberias);
-	
-	// ordeno las listas de ady
-	//~ for(int i = 0; i < n; i++){
-		//~ ady[i].sort();
-	//~ }
-	
-	
 	
 	// ordeno la lista de incidencia por pesos
 	inc.sort(comparar_peso);
 	
-	//~ int bla = 0;
-	//~ for(list< pair<Arista,int> >::iterator it = inc.begin(); it != inc.end(); it++)
-	//~ {
-		//~ bla++;
-	//~ }
-	
+	// armo el plan y devuelvo el costo total del mismo
 	costo_total = petroleo(inc,refinerias,tuberias,n,cant_ref,cant_tub,costo_ref);
+		
+	// salida
+	printf("%i %i %i\n",costo_total,cant_ref,cant_tub);	// K, r, t
 	
-	// cuento la cant de refinerias y guardo los nodos que tendrán refinería
-	//~ cant_ref = comp_conexas(ady,n,refinerias);
-	//~ costo_total = costo_ref*cant_ref;
-	//~ cant_tub = plan(inc,n,tuberias,costo_total);
-	
-	
-	//SALIDA
-	printf("%i %i %i\n",costo_total,cant_ref,cant_tub);
-	
+	// pozos con refinería
 	for (list<int>::iterator it = refinerias.begin(); it != refinerias.end(); it++){
 		printf("%i ",*it + 1);
 	}
 	
+	// tuberías colocadas
 	for (list<Arista>::iterator it = tuberias.begin(); it != tuberias.end(); it++){
 		printf("\n%i %i",it->first + 1,it->second + 1);
 	}
--- TP2/src/petroleo.h
+++ TP2/src/petroleo.h
@@ -1,75 +1,42 @@
 #ifndef PETROLEO_H_INCLUDED
 #define PETROLEO_H_INCLUDED
 
-#include <algorithm>
 #include <list>
 #include <utility>
 #include <vector>
 
 using namespace std;
 
-//~ typedef vector<int> Vec;
-//~ typedef vector<Vec> Grafo;
 
-typedef vector< list<int> > Adyacencia;	// arreglo con los vecinos
-typedef pair<int,int> Arista;	// par de nodos unidos por una arista
-typedef list< pair<Arista,int> > Incidencia;	// lista con pares de arista-peso
+/**************************** ESTRUCTURAS ****************************/
 
+typedef pair<int,int> Arista;	/* par de nodos unidos por una arista */
+typedef list< pair<Arista,int> > Incidencia;	/* lista con pares de arista-peso */
 
 
-//~ int comp_conexas(Grafo g, int n, list<int>& refinerias);
-//~ int petroleo(Grafo g, int n, list< pair<int,int> >& tuberias);
+/********************** DECLARACIÓN DE FUNCIONES **********************/
 
-
-//~ int comp_conexas(Adyacencia ady, int n, list<int>& refinerias);
-//~ void marcar_vecinos(Adyacencia ady, vector<bool>& vistos, int i);
 int plan(Incidencia& inc, int n, list<Arista>& tuberias, int& costo_total,vector<int>& padres);
 bool comparar_peso(const pair<Arista,int>& par1, const pair<Arista,int>& par2);
 int petroleo(Incidencia inc,list<int>& refinerias,list<Arista>& tuberias,int n,int& cant_ref,int& cant_tub);
 
-//~ int comp_conexas(Adyacencia ady, int n, list<int>& refinerias)
-//~ {
-	//~ int res = 0;
-	//~ vector<bool> vistos(n,false);
-	//~ 
-	//~ for(int i = 0; i < n; i++)
-	//~ {
-		//~ if(!vistos[i])
-		//~ {
-			//~ refinerias.push_back(i);
-			//~ res++;
-			//~ marcar_vecinos(ady,vistos,i);
-		//~ }
-	//~ }
-	//~ 
-	//~ return res;
-//~ }
 
-//~ void marcar_vecinos(Adyacencia ady, vector<bool>& vistos, int i)
-//~ {
-	//~ int j;
-	//~ 
-	//~ for(list<int>::iterator it = ady[i].begin(); it != ady[i].end(); it++){
-		//~ j = *it;
-		//~ if(!vistos[j]){
-			//~ marcar_vecinos(ady,vistos,j);
-		//~ }
-	//~ }
-	//~ vistos[i] = true;
-//~ }
+/******************** IMPLEMENTACIÓN DE FUNCIONES ********************/
 
 int plan(Incidencia& inc, int n, list<Arista>& tuberias, int& costo_total,vector<int>& padres)
 {
+/* Función que arma el arbol generador mínimo con las tuberías y
+ * determina las componentes conexas a la vez.  Devuelve la cantidad de
+ * tuberías colocadas.
+ */
 	int costo;
 	int p1;
 	int p2;
 	int res = 0;
 	Arista edge;
-	//~ vector<int> padres(n,-1);
 	vector< pair<int,list<int> > > hijos(n);
 	
-	for(int i = 0; i < n; i++)
-	{
+	for(int i = 0; i < n; i++){	// todos los nodos empiezan con 0 hijos
 		hijos[i].first = 0;
 	}
 	
@@ -81,40 +48,45 @@ int plan(Incidencia& inc, int n, list<Arista>& tuberias, int& costo_total,vector
 		p2 = padres[edge.second];
 		if((p1 != p2) || (p1 == -1))
 		{
-			if(p1 != p2)
+			if(p1 != p2)	// padres distintos
 			{
-				if((p1 != -1) && (p2 != -1))
+				if((p1 != -1) && (p2 != -1))	// si ninguno es -1
 				{
 					if(hijos[p1].first < hijos[p2].first){
 						swap(p1,p2);	// p1 se queda con el que tiene más hijos
 					}
 					
 					for(list<int>::iterator it2 = (hijos[p2].second).begin(); it2 != (hijos[p2].second).end(); it2++)
-					{
+					{	// paso los hijos de p2 como hijos de p1
 						padres[*it2] = p1;
 						(hijos[p1].second).push_back(*it2);
 						hijos[p1].first++;
 					}
+					// pongo a p2 como hijo de p1
 					padres[p2] = p1;
 					(hijos[p1].second).push_back(p2);
 					hijos[p1].first++;
 					
-				}else if(p1 == -1){
+				}else if(p1 == -1){	// si p1 es -1
+					// pongo a p1 como hijo de p2
 					padres[edge.first] = p2;
 					(hijos[p2].second).push_back(edge.first);
 					hijos[p2].first++;
-				}else{
+				}else{	// si p2 es -1
+					// pongo a p2 como hijo de p1
 					padres[edge.second] = p1;
 					(hijos[p1].second).push_back(edge.second);
 					hijos[p1].first++;
 				}
-			}else if (p1 == -1){
+			}else if (p1 == -1){	// si ambos son -1
+				// pongo como padre de ambos a p1
 				padres[edge.first] = edge.first;
 				padres[edge.second] = edge.first;
 				(hijos[edge.first].second).push_back(edge.second);
 				hijos[edge.first].first++;
 			}
 			
+			// actualizo variables
 			costo_total = costo_total + costo;
 			tuberias.push_back(edge);
 			res++;
@@ -129,72 +101,40 @@ bool comparar_peso(const pair<Arista,int>& par1, const pair<Arista,int>& par2)
 	return (par1.second <= par2.second);
 }
 
-
 int petroleo(Incidencia inc,list<int>& refinerias,list<Arista>& tuberias,int n,int& cant_ref,int& cant_tub, int costo_ref)
 {
+/* Función que elabora el plan, determina las refinerías a colocar y 
+ * calcula el costo total.  Devuelve el costo total.
+ */
 	int p;
-	vector<int> padres(n,-1);
-	//~ cant_ref = comp_conexas(ady,n,refinerias);
-	//~ costo_total = costo_ref*cant_ref;
-	//~ cant_ref = 0;
 	int res = 0;
-	cant_tub = plan(inc,n,tuberias,res,padres);
+	vector<int> padres(n,-1);
 	vector<bool> aux(n,false);
+	
+	// elaboro el plan de tuberías
+	cant_tub = plan(inc,n,tuberias,res,padres);
+	
+	// determino refinerías
 	for(int i = 0; i < n; i++)
 	{
 		if(padres[i] == -1)	// nodo solitario
-		{
+		{	// coloco refinería
 			refinerias.push_back(i);
 			cant_ref++;
 		}else{
 			p = padres[i];
-			if(!aux[p])
-			{
+			if(!aux[p])	// nueva componente conexa
+			{	// coloco refinería
 				aux[p] = true;
 				refinerias.push_back(i);
 				cant_ref++;
 			}
 		}
 	}
+	
+	// actualizo costo total
 	res = res + costo_ref*cant_ref;
 	return res;
 }
-
-
-//~ int comp_conexas(Grafo g, int n, list<int, list<int> >& refinerias)
-//~ {
-	//~ int node;
-	//~ int res = 0;
-	//~ list<int> nodos;
-	//~ List<int>::iterator it;
-	//~ 
-	//~ for(int i = 0; i < n; i++){
-		//~ nodos.push_back(i);
-	//~ }
-	//~ 
-	//~ while(!nodos.empty())
-	//~ {
-		//~ it = nodos.begin();
-		//~ node = *it;
-		//~ refinerias.push_back(node);
-		//~ nodos.pop_front();
-		//~ res++;
-		//~ 
-		//~ for(it = nodos.begin(); it != nodos.end(); it++)
-		//~ {
-			//~ if(g[node][*it] != -1){
-				//~ it = nodos.erase(it);
-			//~ }
-		//~ }
-	//~ }
-	//~ 
-	//~ return res;
-//~ }
-//~ 
-//~ int petroleo(Grafo g, int n, list< pair<int,int> >& tuberias)
-//~ {
-	//~ int res = 0;
-//~ }
-//~ 
 
 #endif // PETROLEO_H_INCLUDED
